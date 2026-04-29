af90b87bc6309a28d9cfbdd396dd508dd283832d
Anil.Kumar <anil.kumar@sew.ai>
2026-04-29 19:55:59 +0530

    Quadruple class use demo

---
+++ AndroidPractice/src/main/java/other/QuadrupleDemo.kt
@@ -0,0 +1,13 @@
+package other
+
+/*
+* Quadruple is a generic class that represents a quadruplet of four values.
+* data class Quadruple<out A, out B, out C, out D>(val first: A, val second: B, val third: C, val fourth: D)
+* */
+class QuadrupleDemo {
+}
+
+fun main(){
+    //val quadruple = Quadruple(2, "Kim", true, listOf<String>("Kim","Anil"))
+
+}
