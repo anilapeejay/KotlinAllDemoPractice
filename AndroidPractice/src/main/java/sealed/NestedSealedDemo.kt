bd23596e899ec4f477c8d8375c16f34c2656b9da
Anil.Kumar <anil.kumar@sew.ai>
2026-04-29 20:24:48 +0530

    nested sealed demo - typo mistake

--- AndroidPractice/src/main/java/sealed/NestedSealedDemo.kt
+++ AndroidPractice/src/main/java/sealed/NestedSealedDemo.kt
@@ -6,9 +6,9 @@ package sealed
 sealed class NestedSealedDemo {
     data class Success(val data: String) : NestedSealedDemo()
     sealed class Error : NestedSealedDemo(){
-        object Network : Error()
+        object Network : Error() // object declaration of Error , Singleton (only one instance)
         object Server : Error()
-        data class failer(val message: String) : Error()
+        data class Failure(val message: String) : Error()
     }
 }
 
@@ -17,6 +17,6 @@ fun handleResult(result: NestedSealedDemo){
         NestedSealedDemo.Error.Network -> TODO()
         NestedSealedDemo.Error.Server -> TODO()
         is NestedSealedDemo.Success -> TODO()
-        is NestedSealedDemo.Error.failer -> TODO()
+        is NestedSealedDemo.Error.Failure -> TODO()
     }
 }
